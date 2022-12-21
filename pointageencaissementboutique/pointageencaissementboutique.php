422314c520a1194f7bb468f1e8c6943398eac766
Hugo Doueil <46538211+Ormidales@users.noreply.github.com>
2022-12-21 17:08:18 +0100

    Add files via upload

--- pointageencaissementboutique/pointageencaissementboutique.php
+++ pointageencaissementboutique/pointageencaissementboutique.php
@@ -40,7 +40,7 @@ class PointageEncaissementBoutique extends Module
     {
         $this->name = 'pointageencaissementboutique';
         $this->tab = 'other';
-        $this->version = '1.3.1';
+        $this->version = '1.3.2';
         $this->author = 'Hugo DOUEIL';
         $this->need_instance = 1;
 
